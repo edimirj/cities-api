6b5a8eb68d1ff6435b564e725895a9ca5fd7dcc6
edimirj <edimirj@gmail.com>
2021-05-22 11:04:55 -0300

    Inclusão de estados e cidades na API

--- src/main/java/com/github/edimirj/citiesapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.edimirj.citiesapi.repository;
-
-import com.github.edimirj.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
