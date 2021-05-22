dc375c025d074e806fbebb8b6806097deabd8cd2
edimirj <edimirj@gmail.com>
2021-05-22 13:09:18 -0300

    API finalizada

---
+++ src/main/java/com/github/edimirj/citiesapi/states/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.edimirj.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
--- src/main/java/com/github/edimirj/citiesapi/staties/StateRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.edimirj.citiesapi.staties;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
